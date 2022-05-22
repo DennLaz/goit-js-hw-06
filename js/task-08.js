0c04b05cdbf68e9fb832030d6322fcec01137b48
Denis Lazarenko <dentus25@gmail.com>
2022-05-22 12:29:31 +0300

    add task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginFormEl = document.querySelector(".login-form");
 
+loginFormEl.addEventListener("submit", sendingForm);
+
+function sendingForm(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.target;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled");
+  } else {
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.target.reset();
+  }
+}
