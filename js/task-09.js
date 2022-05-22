5dfb01d84f8a1ecfafe431fdb10afc90ef58acbd
Denis Lazarenko <dentus25@gmail.com>
2022-05-22 12:29:41 +0300

    add task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+
+const spanColorEl = document.querySelector(".color");
+
+bodyEl.addEventListener("click", changeBgColorBody);
+
+function changeBgColorBody(event) {
+  const color = event.currentTarget;
+  color.style.backgroundColor = getRandomHexColor();
+  spanColorEl.textContent = color.style.backgroundColor;
+}
